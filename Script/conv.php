ea884e5a3d32ca6211db060535e67f3845437909
27Cobalter <kontyanzhihg@gmail.com>
2018-12-19 16:23:30 +0900

    [fix]ファイル上書きするように

--- Script/conv.php
+++ Script/conv.php
@@ -8,19 +8,15 @@
 $project='./';
 $files=scandir($project.'pic', 1);
 foreach($files as $num => $file){
-	if(file_exists($project.'resized/'.$file)){
-		echo "ファイルが存在します ".$file."\n";
-	}else{
-		// 元画像のパス
-		$baceImagePath = $project.'pic/'.$file;
-		// 画像保存先のパス(ファイル名)
-		$saveImagePath = $project.'resized/Photo_'.sprintf('%04d', $num+1).'.png';
-		echo 'convert thumb '.$file." to ".$saveImagePath."\n";
-		// インスタンスを生成
-		$image = new Imagick($baceImagePath);
-    $image->resizeImage(1020, 512, Imagick::FILTER_LANCZOS, 0);
-		// 保存
-		$image->writeImage($saveImagePath);
-	}
+  // 元画像のパス
+  $baceImagePath = $project.'pic/'.$file;
+  // 画像保存先のパス(ファイル名)
+  $saveImagePath = $project.'resized/Photo_'.sprintf('%04d', $num+1).'.png';
+  echo 'convert thumb '.$file." to ".$saveImagePath."\n";
+  // インスタンスを生成
+  $image = new Imagick($baceImagePath);
+  $image->resizeImage(1020, 512, Imagick::FILTER_LANCZOS, 0);
+  // 保存
+  $image->writeImage($saveImagePath);
 }
 ?>
